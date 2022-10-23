e7bf12594a8539bcbedfb27066995d5c1d034e94
Juan David <juandavid918@gmail.com>
2022-10-23 16:20:12 -0500

    Seccion fundamentos React finalizada

--- 15. robofriends/src/components/CardList.js
+++ 15. robofriends/src/components/CardList.js
@@ -2,11 +2,15 @@ import React, { Fragment } from 'react';
 
 import Card from './Card';
 
-const CardList = (props) => {
-    const { robots } = props;
+const CardList = ({ robots }) => {
+    // const { robots } = props;
     // const cardsArray = robots.map((user, i) => {
     //     return <Card key={ i } id={ robots[i].id } name={ robots[i].name } email={ robots[i].email }/>
     // });
+
+    // if(true) {
+    //     throw new Error("NOOOOOOOOO!");
+    // }
 
     return (
         <Fragment>
